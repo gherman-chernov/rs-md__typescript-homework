b79e2bc82ca67a6f9402b2dc130c3fb2e2fbdb11
Gherman Chernov <gherman.chernov@gmail.com>
2026-02-15 18:08:41 +0300

    task2 normalizing data

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import { MY_NUMBER } from "./module.js";
+import { posts } from "./tasks/data2.js";
+import normalizeData from "./tasks/task2.js";
 
 
 console.log('Answer is', MY_NUMBER)
+
+console.log(normalizeData(posts))
